044c09bf0064bd39c2c25c97b65a8767d4cd0c8d
Robert Gittings <rjgitting@gmail.com>
2022-06-08 17:43:23 -0400

    commit tree 3d88bb85e92f1410710e9ee868c220cf0ccc51cc

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,28 @@
+class TicTacToe 
+
+        attr_access :board
+
+    initialize
+        @board = ["", "", "", "", "", "". "", "", "", "", ""]
+    end 
+
+    WIN_COMBINATIONS = [
+        [0,1,2],
+        [3,4,5],  
+        [6,7,8],
+        [1,4,7],
+        [2,5,8],
+        [3,6,9],
+        [1,5,9],
+        [3,5,7]
+        ]
+
+    def display_board
+        puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+        puts "-----------"
+        puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+        puts "-----------"
+        puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    end
+
+end 
